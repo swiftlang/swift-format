fe1b26e252c8e7f91d08f8e98ab4da512feadd6a
kitasuke <yusuke2759@gmail.com>
2019-07-20 15:41:47 +0900

    Support SomeTypeSyntax

--- Sources/SwiftFormatPrettyPrint/TokenStreamCreator.swift
+++ Sources/SwiftFormatPrettyPrint/TokenStreamCreator.swift
@@ -1186,6 +1186,11 @@ private final class TokenStreamCreator: SyntaxVisitor {
     return .visitChildren
   }
 
+  func visit(_ node: SomeTypeSyntax) -> SyntaxVisitorContinueKind {
+    after(node.someSpecifier, tokens: .space)
+    return .visitChildren
+  }
+
   func visit(_ node: CompositionTypeSyntax) -> SyntaxVisitorContinueKind {
     return .visitChildren
   }
