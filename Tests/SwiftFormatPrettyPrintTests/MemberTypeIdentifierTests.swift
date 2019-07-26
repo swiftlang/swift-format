5aa4b3a29f481e75666321b37cc60b1c50a6c184
Tony Allevato <allevato@google.com>
2019-07-26 13:56:01 -0700

    Wrap member type identifiers (e.g., `Foo.Bar.Baz<T>`).
    This change also tweaks type annotations slightly, to improve the
    appearance of continuations. In other words, instead of this:
    
    ```swift
    let a:
      One.Two.Three
      .Four
    ```
    
    we do this:
    
    ```swift
    let a:
      One.Two.Three
        .Four
    ```
    
    Since `.Four` is a logical continuation of the line above it, not
    directly of the `let a:` line.

---
+++ Tests/SwiftFormatPrettyPrintTests/MemberTypeIdentifierTests.swift
@@ -0,0 +1,25 @@
+public class MemberTypeIdentifierTests: PrettyPrintTestCase {
+  public func testMemberTypes() {
+    let input =
+      """
+      let a: One.Two.Three.Four.Five
+      let b: One.Two.Three<Four, Five>
+      """
+
+    let expected =
+      """
+      let a:
+        One.Two.Three
+          .Four.Five
+      let b:
+        One.Two
+          .Three<
+            Four,
+            Five
+          >
+
+      """
+
+    assertPrettyPrintEqual(input: input, expected: expected, linelength: 15)
+  }
+}
