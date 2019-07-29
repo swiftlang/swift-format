5237d62180d3ecbad0d2583883c530b1de1db2d8
Tony Allevato <allevato@google.com>
2019-07-29 14:02:30 -0700

    Format `yield` statements.
    Even though these are currently a private feature, if someone wants to
    use them in their code, we should make sure we don't break it by
    formatting them improperly.

---
+++ Tests/SwiftFormatPrettyPrintTests/YieldStmtTests.swift
@@ -0,0 +1,35 @@
+public class YieldStmtTests: PrettyPrintTestCase {
+  public func testBasic() {
+    let input =
+      """
+      var foo: Int {
+        _read{
+          yield 1234567890
+        }
+        _modify{
+          var someLongVariable = 0
+          yield &someLongVariable
+        }
+      }
+      """
+
+    let expected =
+      """
+      var foo: Int {
+        _read {
+          yield
+            1234567890
+        }
+        _modify {
+          var someLongVariable
+            = 0
+          yield
+            &someLongVariable
+        }
+      }
+
+      """
+
+    assertPrettyPrintEqual(input: input, expected: expected, linelength: 19)
+  }
+}
