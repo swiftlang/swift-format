94d849952ab886cdd3fc89b95a9717a7d2cea25f
Dylan Sturgeon <dylansturg@google.com>
2019-11-14 15:49:49 -0800

    Wrap function types in tuples when converting to optional shorthand syntax.
    Fixes https://bugs.swift.org/browse/SR-11771

--- Tests/SwiftFormatRulesTests/UseShorthandTypeNamesTests.swift
+++ Tests/SwiftFormatRulesTests/UseShorthandTypeNamesTests.swift
@@ -11,12 +11,16 @@ public class UseShorthandTypeNamesTests: DiagnosingTestCase {
       input: """
              func enumeratedDictionary<Element>(
                from values: Array<Element>,
-               start: Optional<Array<Element>.Index> = nil
+               start: Optional<Array<Element>.Index> = nil,
+               callback: Optional<(_ someArg: Optional<Bool>) -> Void> = nil
              ) -> Dictionary<Int, Array<Element>> {
                // Specializer syntax
                Array<Array<Optional<Int>>.Index>.init()
                // More specializer syntax
                Array<[Int]>.init()
+               let foo: Optional<() -> Void>? = nil
+               let bar: Array<() -> Void> = []
+               let baz: Dictionary<Int, Optional<(Any) -> Void>>? = nil
              }
              func nestedLongForms(
                x: Array<Dictionary<String, Int>>,
@@ -30,12 +34,16 @@ public class UseShorthandTypeNamesTests: DiagnosingTestCase {
       expected: """
                 func enumeratedDictionary<Element>(
                   from values: [Element],
-                  start: Array<Element>.Index? = nil
+                  start: Array<Element>.Index? = nil,
+                  callback: ((_ someArg: Bool?) -> Void)? = nil
                 ) -> [Int: [Element]] {
                   // Specializer syntax
                   [Array<Int?>.Index].init()
                   // More specializer syntax
                   [[Int]].init()
+                  let foo: (() -> Void)?? = nil
+                  let bar: [() -> Void] = []
+                  let baz: [Int: ((Any) -> Void)?]? = nil
                 }
                 func nestedLongForms(
                   x: [[String: Int]],
