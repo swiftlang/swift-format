90807621e2a52ca3432c56c3bb855bc89146b360
Tony Allevato <allevato@google.com>
2020-08-17 16:52:20 -0700

    Merge pull request #221 from dylansturg/test_name_freedom
    Add an exception to AlwaysUseLowerCamelCase for test names.

--- Sources/SwiftFormatRules/AlwaysUseLowerCamelCase.swift
+++ Sources/SwiftFormatRules/AlwaysUseLowerCamelCase.swift
@@ -19,35 +19,81 @@ import SwiftSyntax
 /// Lint: If an identifier contains underscores or begins with a capital letter, a lint error is
 ///       raised.
 public final class AlwaysUseLowerCamelCase: SyntaxLintRule {
+  /// Stores function decls that are test cases.
+  private var testCaseFuncs = Set<FunctionDeclSyntax>()
+
+  public override func visit(_ node: SourceFileSyntax) -> SyntaxVisitorContinueKind {
+    // Tracks whether "XCTest" is imported in the source file before processing individual nodes.
+    setImportsXCTest(context: context, sourceFile: node)
+    return .visitChildren
+  }
+
+  public override func visit(_ node: ClassDeclSyntax) -> SyntaxVisitorContinueKind {
+    // Check if this class is an `XCTestCase`, otherwise it cannot contain any test cases.
+    guard context.importsXCTest == .importsXCTest else { return .visitChildren }
+
+    // Identify and store all of the function decls that are test cases.
+    let testCases = node.members.members.compactMap {
+      $0.decl.as(FunctionDeclSyntax.self)
+    }.filter {
+      // Filter out non-test methods using the same heuristics as XCTest to identify tests.
+      // Test methods are methods that start with "test", have no arguments, and void return type.
+      $0.identifier.text.starts(with: "test")
+        && $0.signature.input.parameterList.isEmpty
+        && $0.signature.output.map { $0.isVoid } ?? true
+    }
+    testCaseFuncs.formUnion(testCases)
+    return .visitChildren
+  }
+
+  public override func visitPost(_ node: ClassDeclSyntax) {
+    testCaseFuncs.removeAll()
+  }
 
   public override func visit(_ node: VariableDeclSyntax) -> SyntaxVisitorContinueKind {
     for binding in node.bindings {
       guard let pat = binding.pattern.as(IdentifierPatternSyntax.self) else {
         continue
       }
-      diagnoseLowerCamelCaseViolations(pat.identifier)
+      diagnoseLowerCamelCaseViolations(pat.identifier, allowUnderscores: false)
     }
     return .skipChildren
   }
 
   public override func visit(_ node: FunctionDeclSyntax) -> SyntaxVisitorContinueKind {
-    diagnoseLowerCamelCaseViolations(node.identifier)
+    // We allow underscores in test names, because there's an existing convention of using
+    // underscores to separate phrases in very detailed test names.
+    let allowUnderscores = testCaseFuncs.contains(node)
+    diagnoseLowerCamelCaseViolations(node.identifier, allowUnderscores: allowUnderscores)
     return .skipChildren
   }
 
   public override func visit(_ node: EnumCaseElementSyntax) -> SyntaxVisitorContinueKind {
-    diagnoseLowerCamelCaseViolations(node.identifier)
+    diagnoseLowerCamelCaseViolations(node.identifier, allowUnderscores: false)
     return .skipChildren
   }
 
-  private func diagnoseLowerCamelCaseViolations(_ identifier: TokenSyntax) {
+  private func diagnoseLowerCamelCaseViolations(_ identifier: TokenSyntax, allowUnderscores: Bool) {
     guard case .identifier(let text) = identifier.tokenKind else { return }
     if text.isEmpty { return }
-    if text.dropFirst().contains("_") || ("A"..."Z").contains(text.first!) {
+    if (text.dropFirst().contains("_") && !allowUnderscores) || ("A"..."Z").contains(text.first!) {
       diagnose(.variableNameMustBeLowerCamelCase(text), on: identifier) {
         $0.highlight(identifier.sourceRange(converter: self.context.sourceLocationConverter))
       }
     }
+  }
+}
+
+extension ReturnClauseSyntax {
+  /// Whether this return clause specifies an explicit `Void` return type.
+  fileprivate var isVoid: Bool {
+    if let returnTypeIdentifier = returnType.as(SimpleTypeIdentifierSyntax.self) {
+      return returnTypeIdentifier.name.text == "Void"
+    }
+    if let returnTypeTuple = returnType.as(TupleTypeSyntax.self) {
+      return returnTypeTuple.elements.isEmpty
+    }
+    return false
   }
 }
 
