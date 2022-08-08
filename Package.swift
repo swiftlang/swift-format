5a334db00ccc1e94e7b889962348e0229d3da0cd
Stephen Celis <stephen.celis@gmail.com>
2022-08-08 13:10:58 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.4
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
