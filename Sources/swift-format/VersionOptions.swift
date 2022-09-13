40b740826d0aeb7af226ae07589f03d2f9a74ae6
Tony Allevato <allevato@google.com>
2022-09-13 09:14:03 -0700

    Update package and version number for 0.50700.0 release.

--- Sources/swift-format/VersionOptions.swift
+++ Sources/swift-format/VersionOptions.swift
@@ -20,7 +20,7 @@ struct VersionOptions: ParsableArguments {
   func validate() throws {
     if version {
       // TODO: Automate updates to this somehow.
-      print("0.50500.0")
+      print("0.50700.0")
       throw ExitCode.success
     }
   }
