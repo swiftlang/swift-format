59fad250f8dce9eb12784755d1fb92e98fc31744
Dave Abrahams <dabrahams@adobe.com>
2022-12-12 16:57:49 -0800

    Update to swift-syntax main branch

--- Package.swift
+++ Package.swift
@@ -219,7 +219,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     ),
     .package(
       url: "https://github.com/apple/swift-syntax.git",
-      .upToNextMinor(from: "0.50700.0")
+      branch: "main"
     ),
     .package(
       url: "https://github.com/apple/swift-tools-support-core.git",
