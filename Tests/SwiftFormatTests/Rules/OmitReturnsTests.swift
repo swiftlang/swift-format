fcdf5ee2700b3e9021751d019b3e021213b0f16e
Pavel Yaskevich <xedin@apache.org>
2023-08-21 12:10:31 -0700

    Adjust OmitReturnsTests to use the most recent APIs

--- Tests/SwiftFormatTests/Rules/OmitReturnsTests.swift
+++ Tests/SwiftFormatTests/Rules/OmitReturnsTests.swift
@@ -1,105 +1,107 @@
+import _SwiftFormatTestSupport
+
 @_spi(Rules) import SwiftFormat
 
 final class OmitReturnsTests: LintOrFormatRuleTestCase {
   func testOmitReturnInFunction() {
-    XCTAssertFormatting(
+    assertFormatting(
       OmitReturns.self,
       input: """
         func test() -> Bool {
-          return false
+          1️⃣return false
         }
-        """,
+      """,
       expected: """
         func test() -> Bool {
           false
         }
-        """)
+      """,
+      findings: [
+        FindingSpec("1️⃣", message: "`return` can be omitted because body consists of a single expression")
+      ])
   }
 
   func testOmitReturnInClosure() {
-    XCTAssertFormatting(
+    assertFormatting(
       OmitReturns.self,
       input: """
         vals.filter {
-          return $0.count == 1
+          1️⃣return $0.count == 1
         }
-        """,
+      """,
       expected: """
         vals.filter {
           $0.count == 1
         }
-        """)
+      """,
+      findings: [
+        FindingSpec("1️⃣", message: "`return` can be omitted because body consists of a single expression")
+      ])
   }
 
   func testOmitReturnInSubscript() {
-      XCTAssertFormatting(
-        OmitReturns.self,
-        input: """
-          struct Test {
-            subscript(x: Int) -> Bool {
-              return false
-            }
-          }
-          """,
-        expected: """
-          struct Test {
-            subscript(x: Int) -> Bool {
-              false
-            }
-          }
-          """)
-
-      XCTAssertFormatting(
-        OmitReturns.self,
-        input: """
-          struct Test {
-            subscript(x: Int) -> Bool {
-              get {
-                return false
-              }
-              set { }
-            }
-          }
-          """,
-        expected: """
-          struct Test {
-            subscript(x: Int) -> Bool {
-              get {
-                false
-              }
-              set { }
-            }
-          }
-          """)
-  }
-
-  func testOmitReturnInComputedVars() {
-    XCTAssertFormatting(
-      OmitReturns.self,
-      input: """
-        var x: Int {
-          return 42
-        }
-        """,
-      expected: """
-        var x: Int {
-          42
-        }
-        """)
-
-    XCTAssertFormatting(
+    assertFormatting(
       OmitReturns.self,
       input: """
         struct Test {
-          var x: Int {
+          subscript(x: Int) -> Bool {
+            1️⃣return false
+          }
+        }
+
+        struct Test {
+          subscript(x: Int) -> Bool {
             get {
-              return 42
+              2️⃣return false
             }
             set { }
           }
         }
-        """,
+      """,
       expected: """
+        struct Test {
+          subscript(x: Int) -> Bool {
+            false
+          }
+        }
+
+        struct Test {
+          subscript(x: Int) -> Bool {
+            get {
+              false
+            }
+            set { }
+          }
+        }
+      """,
+      findings: [
+        FindingSpec("1️⃣", message: "`return` can be omitted because body consists of a single expression"),
+        FindingSpec("2️⃣", message: "`return` can be omitted because body consists of a single expression")
+      ])
+  }
+
+  func testOmitReturnInComputedVars() {
+    assertFormatting(
+      OmitReturns.self,
+      input: """
+        var x: Int {
+          1️⃣return 42
+        }
+
+        struct Test {
+          var x: Int {
+            get {
+              2️⃣return 42
+            }
+            set { }
+          }
+        }
+      """,
+      expected: """
+        var x: Int {
+          42
+        }
+
         struct Test {
           var x: Int {
             get {
@@ -108,6 +110,10 @@ final class OmitReturnsTests: LintOrFormatRuleTestCase {
             set { }
           }
         }
-        """)
+      """,
+      findings: [
+        FindingSpec("1️⃣", message: "`return` can be omitted because body consists of a single expression"),
+        FindingSpec("2️⃣", message: "`return` can be omitted because body consists of a single expression")
+      ])
   }
 }
