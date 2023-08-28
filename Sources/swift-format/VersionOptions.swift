ca2e712551e02aaafcc76d1fcbb76ccc61b2a1db
Pedro Alcobia <102952330+PedroAlcobia-TomTom@users.noreply.github.com>
2023-08-28 15:11:51 +0200

    bump version.

--- Sources/swift-format/VersionOptions.swift
+++ Sources/swift-format/VersionOptions.swift
@@ -20,7 +20,7 @@ struct VersionOptions: ParsableArguments {
   func validate() throws {
     if version {
       // TODO: Automate updates to this somehow.
-      print("508.0.2")
+      print("508.0.3")
       throw ExitCode.success
     }
   }
