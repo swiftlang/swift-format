555ea4825e01cb0941cbc491486205e67ce34bb3
Tony Allevato <allevato@google.com>
2023-09-14 16:01:48 -0400

    Format `discard` statements.

---
+++ Tests/SwiftFormatTests/PrettyPrint/DiscardStmtTests.swift
@@ -0,0 +1,13 @@
+final class DiscardStmtTests: PrettyPrintTestCase {
+  func testDiscard() {
+    assertPrettyPrintEqual(
+      input: """
+        discard self
+        """,
+      expected: """
+        discard self
+
+        """,
+      linelength: 9)
+  }
+}
