13e5246d4d8f34df3d9b05a1919951bdedb915ca
Tony Allevato <allevato@google.com>
2023-09-14 16:10:06 -0400

    Format `copy` expressions.

---
+++ Tests/SwiftFormatTests/PrettyPrint/CopyExprSyntax.swift
@@ -0,0 +1,14 @@
+final class CopyExprTests: PrettyPrintTestCase {
+  func testCopy() {
+    assertPrettyPrintEqual(
+      input: """
+        let x = copy y
+        """,
+      expected: """
+        let x =
+          copy y
+
+        """,
+      linelength: 13)
+  }
+}
