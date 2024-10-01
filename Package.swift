ac59447d50c6a7e14126604d1454b477fc1cd789
swift-ci <swift-ci@users.noreply.github.com>
2024-10-01 03:43:57 +0000

    Change swift-syntax dependency to 601.0.0-prerelease-2024-09-25

--- Package.swift
+++ Package.swift
@@ -166,7 +166,7 @@ var dependencies: [Package.Dependency] {
     return [
       .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
       .package(url: "https://github.com/apple/swift-markdown.git", from: "0.2.0"),
-      .package(url: "https://github.com/swiftlang/swift-syntax.git", branch: "main"),
+      .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0-prerelease-2024-09-25"),
     ]
   }
 }
