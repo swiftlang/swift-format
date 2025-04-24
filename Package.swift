2ac0eee73265bcb2b16100651207b374dc92a966
Alex Hoppen <ahoppen@apple.com>
2025-04-24 16:48:27 +0200

    Add a missing dependency from `swift-format` to `SwiftDiagnostics`
    SwiftDiagnostics is imported in swift-format but we didn’t declare a dependency edge in the package manifest.

--- Package.swift
+++ Package.swift
@@ -96,7 +96,7 @@ var targets: [Target] = [
       "_SwiftFormatInstructionCounter",
       "SwiftFormat",
       .product(name: "ArgumentParser", package: "swift-argument-parser"),
-    ] + swiftSyntaxDependencies(["SwiftParser", "SwiftSyntax"]),
+    ] + swiftSyntaxDependencies(["SwiftDiagnostics", "SwiftParser", "SwiftSyntax"]),
     exclude: ["CMakeLists.txt"],
     linkerSettings: swiftformatLinkSettings
   ),
